bd2a0b735f55ac1a627b27483fcffabe310e810e
roman <kaossilat0r.rn@gmail.com>
2015-10-18 19:47:20 +0200

    generated AngularJS project with yo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+angular.module('photoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
