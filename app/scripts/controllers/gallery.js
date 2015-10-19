51782847881f5b26fd5f8ccb71c05b509b646515
roman <kaossilat0r.rn@gmail.com>
2015-10-19 13:52:43 +0200

    show received pictures in a primitive list

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -5,9 +5,22 @@ angular.module('photoApp')
   .controller('GalleryController', [
     'galleryService',
     '$scope',
-    function (galleryService,$scope) {
+    '$http',
+    'config',
+    function (galleryService,$scope,$http,config) {
 
       $scope.galleryService = galleryService;
+
+      $scope.pictures= [];
+      $http.get(config.webservice + "/user/" + $scope.galleryService.selectedUser + "/photo")
+        .success(function(data, status) {
+          $scope.pictures = data;
+          console.log(data);
+        })
+        .error(function(data, status) {
+          console.log("GET /users failed: " + status);
+        });
+
 
       this.awesomeThings = [
         'HTML5 Boilerplate',
